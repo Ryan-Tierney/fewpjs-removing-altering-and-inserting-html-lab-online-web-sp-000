24e73ada712b45074463488b6c2421e4520c7e75
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-16 22:46:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
+newHeader.innerHTML = "Ryan Tierney is the champion" 
+newHeader.class = "victory"
